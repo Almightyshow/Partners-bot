b0393c4c9ac72910b9ae2b644de8f5908939830b
Almightyshow <141552089+Almightyshow@users.noreply.github.com>
2023-08-06 12:19:49 +0100

    config.js

--- src/data/config/config.js
+++ src/data/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   token:
-    "TOKEN_HERE",
-  botID: "BOT_ID_HERE",
-  guildID: "PRIVATE_SERVER_ID_HERE",
+    "MTEzNzY5ODEzNjk3NDk2Njg2Ng.G2lcxV.Id1xYgGVSQhoF4fQLq4uUHYN3untf7bjdDJacA",
+  botID: "1137698136974966866",
+  guildID: "1136229343501225997",
 };
